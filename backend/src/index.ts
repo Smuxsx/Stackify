16fb8a89de8416ae24cd454d604eafbd07430a68
Smuxsx <eabraham.espinoza@gmail.com>
2026-06-12 17:04:25 -0600

    Add .gitignore file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
 import express from "express"
+import cors from "cors"
 import { ENV } from "./config/env.ts"
 import { clerkMiddleware } from "@clerk/express"
 
 const app = express()
 
-app.use(clerkMiddleware())
+app.use(cors({origin: ENV.FRONTEND_URL})) // Enable CORS for the frontend URL specified in the environment variables. This allows our frontend to make requests to this backend without CORS issues.
+app.use(clerkMiddleware()) // Auth obj will be available in req.auth after this middleware.
+app.use(express.json()) // parses JSON request bodies.
+app.use(express.urlencoded({extended: true})); // parses form data (Like HTML forms).
+
 
 app.get("/", (req, res) => {
     res.json({
